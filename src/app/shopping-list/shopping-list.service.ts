2d767720d760e92e5163b6e39b2d4250530ca3fc
Emilia Duniewska <kamienie@gmail.com>
2022-12-19 12:08:48 +0100

    Correct the typo

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -32,8 +32,8 @@ export class ShoppingListService {
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 
-  updateIngredient(index: number, newIgredient: Ingredient) {
-    this.ingredients[index] = newIgredient;
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 
